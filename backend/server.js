c732dc846bc72634f85e240d722df21bb4da5a20
Windsor Nguyen <mn4560@princeton.edu>
2022-10-23 21:58:59 -0400

    started server

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // Allows us to parse .json
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });

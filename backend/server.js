56abd311ab64e0bb74a1aa8b0041057f8004adf3
Windsor Nguyen <mn4560@princeton.edu>
2022-10-23 21:56:03 -0400

    started server

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// Require all the things we need
+const express = require('express');
+const cors = require('cors');
+
+// Configures our environment variables in the dotenv file
+require('dotenv').config();
+
+// Create express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Allows us to parse .json
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});

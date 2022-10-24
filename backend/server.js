a472d7798174878d6141fd6ebaa7f4cb0a8b00ea
Windsor Nguyen <mn4560@princeton.edu>
2022-10-23 22:05:19 -0400

    started server

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // Allows us to parse .json
 
+// In the event the app crashes:
+process.once('SIGUSR2', function () {
+  process.kill(process.pid, 'SIGUSR2');
+});
+
+process.on('SIGINT', function () {
+  // This is only called on ctrl+c, not restart
+  process.kill(process.pid, 'SIGINT');
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
